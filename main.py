2ecdec60216dae785c6827a4ebb2a88383ec8b3f
Bobirjon <bobirjonmuxammadov20314@gmail.com>
2026-04-20 16:48:53 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bobirjonmuxammadov20314-dev/Email.git
+git push -u origin main
+
+"""
